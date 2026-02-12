6671f598840c345df572a20f299ea0b9ab5bf895
Dawley Felix <felixdawley27@gmail.com>
2026-02-12 17:18:35 -0600

    Add day 1 variables practice - band name generator

---
+++ Band_name_generator.py
@@ -0,0 +1,4 @@
+print("welcome to the Band Name Generator by Dawley Felix.")
+city = input("Which city did you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name coul be: " + city + " " + pet)
